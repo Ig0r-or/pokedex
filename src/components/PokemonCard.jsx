691ba0a554d3f48cd709abba091119a390ad48f7
Ig0r_or <igorgabriel111105@gmail.com>
2026-05-13 19:23:14 -0300

    Atualizando pra criar um molde de pokemon cards

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,26 +1,12 @@
-function PokemonCard() {
+function PokemonCard({id, name, type, imgUrl}) {
   return (
     <div>
       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-        alt="Pikachu"
+        src={imgUrl}
+        alt={name}
       />
-      <h2>#025 — Pikachu</h2>
-      <p>Tipo: Elétrico</p>
-
-       <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/212.png"
-        alt="Pikachu"
-      />
-      <h2>#212 — Scizor</h2>
-      <p>Tipo: Estrela</p>
-
-        <img
-        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png"
-        alt="Pikachu"
-      />
-      <h2>#024 — Bixo Cobra</h2>
-      <p>Tipo: Veneno</p>
+      <h2>#{String(id).padStart(3, '0')} — {name}</h2>
+      <p>Tipo: {type}</p>
     </div>
   )
 }
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -3,7 +3,24 @@ import PokemonCard from './PokemonCard'
 function PokemonList() {
   return (
     <section>
-      <PokemonCard />
+      <PokemonCard 
+        id={25}
+        name="Pikachu"
+        type="Elétrico"
+        imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+      />
+      <PokemonCard 
+        id={212}
+        name="Scizor"
+        type="Inseto"
+        imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/212.png"
+      />
+        <PokemonCard
+        id={24}
+        name="Bixo Cobra"
+        type="Veneno"
+        imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png"
+      />
     </section>
   )
 }

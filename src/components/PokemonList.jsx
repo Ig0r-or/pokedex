c8ccca3f2757d21147e6b90e1f9ed191d1fb9e85
Ig0r_or <igorgabriel111105@gmail.com>
2026-05-13 20:05:18 -0300

    adicionando peripécias

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,27 +1,56 @@
 import PokemonCard from './PokemonCard'
+import { useState } from 'react'
 
-function PokemonList() {
+const POKEMONS = [ 
+        {id: 25,
+        name: 'Pikachu',
+        type: 'Elétrico',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png'
+       },
+        {id: 212,
+        name: 'Scizor',
+        type: 'Estrela',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/212.png'
+        },
+        {id: 24,
+        name: 'Bixo Cobra',
+        type: 'Veneno',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png'
+        }
+       ]
+
+  function PokemonList() {
+
+  const [pokemons] = useState(POKEMONS)
+  const [filtro, setFiltro] = useState('')
+
+  const listaFiltrada = pokemons.filter(pokemon => 
+    pokemon.name.toLowerCase().includes(filtro.toLowerCase())
+
+  )
+
   return (
     <section>
-      <PokemonCard 
-        id={25}
-        name="Pikachu"
-        type="Elétrico"
-        imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
-      />
-      <PokemonCard 
-        id={212}
-        name="Scizor"
-        type="Inseto"
-        imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/212.png"
-      />
-        <PokemonCard
-        id={24}
-        name="Bixo Cobra"
-        type="Veneno"
-        imgUrl="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/24.png"
-      />
-    </section>
+    <label htmlFor="busca">Buscar por nome: </label>
+    <input
+        id="busca"
+        type="search"
+        value={filtro}
+        onChange={(e) => setFiltro(e.target.value)}
+        placeholder="Ex.: char"
+ />
+    {listaFiltrada.map((pokemon) => (
+    <PokemonCard 
+      key={pokemon.id}
+      id={pokemon.id}
+      name={pokemon.name}
+      type={pokemon.type}
+      imgUrl={pokemon.imageUrl}
+    />
+     
+))}
+ </section>
+
   )
 }
 

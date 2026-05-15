35ec6ff15260feb88913b308e813d79ba5d340aa
Ig0r_or <igorgabriel111105@gmail.com>
2026-05-14 21:49:29 -0300

    Implement code changes to enhance functionality and improve performance

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,5 +1,6 @@
 import PokemonCard from './PokemonCard'
 import { useState } from 'react'
+import neymarImg from '../assets/neymar.png'
 
 const POKEMONS = [ 
         {id: 25,
@@ -26,8 +27,38 @@ const POKEMONS = [
         name: 'Bulbasaur',
         type: 'Grama',
         imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
+        },
+        {id: 69,
+        name: 'Bellsprout',
+        type: 'Grama',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/69.png'
+        },
+        {id: 320,
+        name: 'Wailmer',
+        type: 'Água',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/320.png'
+        },
+        {id: 150,
+        name: 'Mewtwo',
+        type: 'Psíquico',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png'
+        },
+        {id: 143,
+        name: 'Snorlax',
+        type: 'Normal',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/143.png'
+        },
+        {id: 94,
+        name: 'Gengar',
+        type: 'Fantasma',
+        imageUrl: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png'
+        },
+        {id: 10,
+         name: 'Neymar Jr',
+         type: 'Futebolista',
+         imageUrl: neymarImg
         }
-       ]
+      ]
 
   function PokemonList() {
 
